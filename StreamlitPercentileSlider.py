1faa914e5537bf5e4828e425b9968411b93653a0
ecowart99 <93290582+ecowart99@users.noreply.github.com>
2022-05-26 16:20:58 -0400

    Update StreamlitPercentileSlider.py

--- StreamlitPercentileSlider.py
+++ StreamlitPercentileSlider.py
@@ -3,3 +3,6 @@ import pandas as pd
 
 maxPercent = st.slider('Maximum Percentile', 0, 100)
 minPercent = st.slider('Minimum Percentile', 0, 100)
+
+cooling = pd.read_csv('smallCoolingData.csv')
+st.bar_chart(cooling)
