5e1112ed03a239a6564606eb8b844316e02280e6
ecowart99 <93290582+ecowart99@users.noreply.github.com>
2022-05-31 12:27:27 -0400

    Update StreamlitPercentileSlider.py

--- StreamlitPercentileSlider.py
+++ StreamlitPercentileSlider.py
@@ -6,7 +6,7 @@ import numpy as np
 maxPercent = st.slider('Maximum Percentile', 0, 100)
 minPercent = st.slider('Minimum Percentile', 0, 100)
 
-cooling = pd.read_csv('smallCoolingData.csv')
+cooling = pd.read_csv('C:/Users/ecowart/SmithGroup Companies Inc/Internal Projects/WebApps/Streamlit/smallCoolingData.csv')
 
 histBins = np.arange(cooling['LoadHourlyAvg'].min(), cooling['LoadHourlyAvg'].nlargest(50).iloc[-1], 100)
 fig, ax = plt.subplots(figsize=(12,6), facecolor='w')
@@ -14,6 +14,6 @@ cnts, values, bars = ax.hist(cooling['LoadHourlyAvg'], edgecolor='k', bins = his
 
 plt.xlabel('Cooling Load (Btu/h)', fontsize = 18)
 plt.ylabel('Count of Hours', fontsize = 18)
-plt.title('Histogram of San Leandro Patient Room btu/h', fontsize = 24)
+plt.title('Histogram of San Leandro Patient Room Btu/h', fontsize = 24)
 
-fig.show()
+st.pyplot(fig)
